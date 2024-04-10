2c1182f9bc526eef0f10f40c78e40d0bc23361ad
Emil57 <trejocovantesde@gmail.com>
2024-04-10 16:47:05 -0700

    Comment code for debug only

--- report_app/run.py
+++ report_app/run.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 PATHS_TO_SEARCH = [
-    '/Users/DimasEmiliano/Documents/VisualStudioCode/aoi-reports/report_app/tests', #debug purposes, don´t delete
+    #'/Users/DimasEmiliano/Documents/VisualStudioCode/aoi-reports/report_app/tests', #debug purposes, don´t delete
     '\\\\mex6vtrx01\\Texas\\Report\\ICPLUS',
     '\\\\mex6vtrx02\\Texas\\Report\\ICPLUS',
     ]
@@ -188,9 +188,9 @@ def main(start_date: datetime.date, end_date: datetime.date) -> str | None:
             if start_date <= last_modified_time <= end_date:
                 list_files.append(file_path)
         #---- DEBUG PURPOSES, DON´T REVOVE ----
-            if len(list_files) == 2:
-                break
-        break
+            #if len(list_files) == 2:
+            #    break
+        #break
         #--------------------------------------
 
     print('Generating report...')
